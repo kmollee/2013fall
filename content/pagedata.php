e68da510ebd5e73093ee94524d17d702222fc846
kmollee <10073105@gm.nfu.edu.tw>
2013-06-07 01:42:49 +0800

    cmsimple add

---
+++ content/pagedata.php
@@ -0,0 +1,436 @@
+<?php 
+/* utf8-marker = äöüß */ 
+################## Data fields ############
+$page_data_fields[] = 'url';
+$page_data_fields[] = 'last_edit';
+$page_data_fields[] = 'description';
+$page_data_fields[] = 'keywords';
+$page_data_fields[] = 'title';
+$page_data_fields[] = 'robots';
+$page_data_fields[] = 'heading';
+$page_data_fields[] = 'show_heading';
+$page_data_fields[] = 'template';
+$page_data_fields[] = 'published';
+$page_data_fields[] = 'show_last_edit';
+$page_data_fields[] = 'linked_to_menu';
+$page_data_fields[] = 'header_location';
+$page_data_fields[] = 'use_header_location';
+
+################## Recently deleted ############
+$temp_data['url'] = '%E5%BC%95%E7%94%A8';
+$temp_data['last_edit'] = '1349832593';
+$temp_data['description'] = '';
+$temp_data['keywords'] = '';
+$temp_data['title'] = '';
+$temp_data['robots'] = '';
+$temp_data['heading'] = '';
+$temp_data['show_heading'] = '';
+$temp_data['template'] = '';
+$temp_data['published'] = '';
+$temp_data['show_last_edit'] = '';
+$temp_data['linked_to_menu'] = '';
+$temp_data['header_location'] = '';
+$temp_data['use_header_location'] = '';
+
+################## Page Data ############
+$page_data[0]['url'] = 'CD2';
+$page_data[0]['last_edit'] = '1370417688';
+$page_data[0]['description'] = '';
+$page_data[0]['keywords'] = '';
+$page_data[0]['title'] = '';
+$page_data[0]['robots'] = '';
+$page_data[0]['heading'] = '';
+$page_data[0]['show_heading'] = '';
+$page_data[0]['template'] = '';
+$page_data[0]['published'] = '';
+$page_data[0]['show_last_edit'] = '';
+$page_data[0]['linked_to_menu'] = '';
+$page_data[0]['header_location'] = '';
+$page_data[0]['use_header_location'] = '';
+
+//----------
+$page_data[1]['url'] = 'w15';
+$page_data[1]['last_edit'] = '1370417828';
+$page_data[1]['description'] = '';
+$page_data[1]['keywords'] = '';
+$page_data[1]['title'] = '';
+$page_data[1]['robots'] = '';
+$page_data[1]['heading'] = '';
+$page_data[1]['show_heading'] = '';
+$page_data[1]['template'] = '';
+$page_data[1]['published'] = '';
+$page_data[1]['show_last_edit'] = '';
+$page_data[1]['linked_to_menu'] = '';
+$page_data[1]['header_location'] = '';
+$page_data[1]['use_header_location'] = '';
+
+//----------
+$page_data[2]['url'] = 'C2';
+$page_data[2]['last_edit'] = '1370515337';
+$page_data[2]['description'] = '';
+$page_data[2]['keywords'] = '';
+$page_data[2]['title'] = '';
+$page_data[2]['robots'] = '';
+$page_data[2]['heading'] = '';
+$page_data[2]['show_heading'] = '';
+$page_data[2]['template'] = '';
+$page_data[2]['published'] = '';
+$page_data[2]['show_last_edit'] = '';
+$page_data[2]['linked_to_menu'] = '';
+$page_data[2]['header_location'] = '';
+$page_data[2]['use_header_location'] = '';
+
+//----------
+$page_data[3]['url'] = 'w15';
+$page_data[3]['last_edit'] = '1370516230';
+$page_data[3]['description'] = '';
+$page_data[3]['keywords'] = '';
+$page_data[3]['title'] = '';
+$page_data[3]['robots'] = '';
+$page_data[3]['heading'] = '';
+$page_data[3]['show_heading'] = '';
+$page_data[3]['template'] = '';
+$page_data[3]['published'] = '';
+$page_data[3]['show_last_edit'] = '';
+$page_data[3]['linked_to_menu'] = '';
+$page_data[3]['header_location'] = '';
+$page_data[3]['use_header_location'] = '';
+
+//----------
+$page_data[4]['url'] = '%E6%AD%A1%E8%BF%8E%E8%92%9E%E8%87%A8_CMSimple_XH_%E5%8F%B0%E7%81%A3%E7%B9%81%E9%AB%94%E7%89%88';
+$page_data[4]['last_edit'] = '1370433836';
+$page_data[4]['description'] = '';
+$page_data[4]['keywords'] = '';
+$page_data[4]['title'] = '';
+$page_data[4]['robots'] = '';
+$page_data[4]['heading'] = '';
+$page_data[4]['show_heading'] = '0';
+$page_data[4]['template'] = '0';
+$page_data[4]['published'] = '1';
+$page_data[4]['show_last_edit'] = '0';
+$page_data[4]['linked_to_menu'] = '1';
+$page_data[4]['header_location'] = '';
+$page_data[4]['use_header_location'] = '0';
+
+//----------
+$page_data[5]['url'] = 'myplugin';
+$page_data[5]['last_edit'] = '1352511712';
+$page_data[5]['description'] = '';
+$page_data[5]['keywords'] = '';
+$page_data[5]['title'] = '';
+$page_data[5]['robots'] = '';
+$page_data[5]['heading'] = '';
+$page_data[5]['show_heading'] = '';
+$page_data[5]['template'] = '';
+$page_data[5]['published'] = '';
+$page_data[5]['show_last_edit'] = '';
+$page_data[5]['linked_to_menu'] = '';
+$page_data[5]['header_location'] = '';
+$page_data[5]['use_header_location'] = '';
+
+//----------
+$page_data[6]['url'] = 'Axuploader';
+$page_data[6]['last_edit'] = '1352511000';
+$page_data[6]['description'] = '';
+$page_data[6]['keywords'] = '';
+$page_data[6]['title'] = '';
+$page_data[6]['robots'] = '';
+$page_data[6]['heading'] = '';
+$page_data[6]['show_heading'] = '0';
+$page_data[6]['template'] = '0';
+$page_data[6]['published'] = '1';
+$page_data[6]['show_last_edit'] = '0';
+$page_data[6]['linked_to_menu'] = '1';
+$page_data[6]['header_location'] = '';
+$page_data[6]['use_header_location'] = '0';
+
+//----------
+$page_data[7]['url'] = 'Menu_Levels_%26amp%3B_Headings';
+$page_data[7]['last_edit'] = '1317313940';
+$page_data[7]['description'] = '';
+$page_data[7]['keywords'] = '';
+$page_data[7]['title'] = '';
+$page_data[7]['robots'] = '';
+$page_data[7]['heading'] = '';
+$page_data[7]['show_heading'] = '0';
+$page_data[7]['template'] = '0';
+$page_data[7]['published'] = '1';
+$page_data[7]['show_last_edit'] = '0';
+$page_data[7]['linked_to_menu'] = '1';
+$page_data[7]['header_location'] = '';
+$page_data[7]['use_header_location'] = '0';
+
+//----------
+$page_data[8]['url'] = 'Menu_Level_2_%E2%80%93_Page_1';
+$page_data[8]['last_edit'] = '1311879528';
+$page_data[8]['description'] = '';
+$page_data[8]['keywords'] = '';
+$page_data[8]['title'] = '';
+$page_data[8]['robots'] = '';
+$page_data[8]['heading'] = '';
+$page_data[8]['show_heading'] = '';
+$page_data[8]['template'] = '';
+$page_data[8]['published'] = '';
+$page_data[8]['show_last_edit'] = '';
+$page_data[8]['linked_to_menu'] = '1';
+$page_data[8]['header_location'] = '';
+$page_data[8]['use_header_location'] = '';
+
+//----------
+$page_data[9]['url'] = 'Menu_Level_3_%E2%80%93_Page_1';
+$page_data[9]['last_edit'] = '1311881845';
+$page_data[9]['description'] = '';
+$page_data[9]['keywords'] = '';
+$page_data[9]['title'] = '';
+$page_data[9]['robots'] = '';
+$page_data[9]['heading'] = '';
+$page_data[9]['show_heading'] = '';
+$page_data[9]['template'] = '';
+$page_data[9]['published'] = '';
+$page_data[9]['show_last_edit'] = '';
+$page_data[9]['linked_to_menu'] = '1';
+$page_data[9]['header_location'] = '';
+$page_data[9]['use_header_location'] = '';
+
+//----------
+$page_data[10]['url'] = 'Menu_Level_3_%E2%80%93_Page_2';
+$page_data[10]['last_edit'] = '1311883077';
+$page_data[10]['description'] = '';
+$page_data[10]['keywords'] = '';
+$page_data[10]['title'] = '';
+$page_data[10]['robots'] = '';
+$page_data[10]['heading'] = '';
+$page_data[10]['show_heading'] = '';
+$page_data[10]['template'] = '';
+$page_data[10]['published'] = '';
+$page_data[10]['show_last_edit'] = '';
+$page_data[10]['linked_to_menu'] = '1';
+$page_data[10]['header_location'] = '';
+$page_data[10]['use_header_location'] = '';
+
+//----------
+$page_data[11]['url'] = 'Menu_Level_3_%E2%80%93_Page_3';
+$page_data[11]['last_edit'] = '1311881854';
+$page_data[11]['description'] = '';
+$page_data[11]['keywords'] = '';
+$page_data[11]['title'] = '';
+$page_data[11]['robots'] = '';
+$page_data[11]['heading'] = '';
+$page_data[11]['show_heading'] = '';
+$page_data[11]['template'] = '';
+$page_data[11]['published'] = '';
+$page_data[11]['show_last_edit'] = '';
+$page_data[11]['linked_to_menu'] = '1';
+$page_data[11]['header_location'] = '';
+$page_data[11]['use_header_location'] = '';
+
+//----------
+$page_data[12]['url'] = 'Menu_Level_2_%E2%80%93_Page_2';
+$page_data[12]['last_edit'] = '1311879713';
+$page_data[12]['description'] = '';
+$page_data[12]['keywords'] = '';
+$page_data[12]['title'] = '';
+$page_data[12]['robots'] = '';
+$page_data[12]['heading'] = '';
+$page_data[12]['show_heading'] = '';
+$page_data[12]['template'] = '';
+$page_data[12]['published'] = '';
+$page_data[12]['show_last_edit'] = '';
+$page_data[12]['linked_to_menu'] = '1';
+$page_data[12]['header_location'] = '';
+$page_data[12]['use_header_location'] = '';
+
+//----------
+$page_data[13]['url'] = 'Menu_Level_3_%E2%80%93_Page_1';
+$page_data[13]['last_edit'] = '1311882970';
+$page_data[13]['description'] = '';
+$page_data[13]['keywords'] = '';
+$page_data[13]['title'] = '';
+$page_data[13]['robots'] = '';
+$page_data[13]['heading'] = '';
+$page_data[13]['show_heading'] = '';
+$page_data[13]['template'] = '';
+$page_data[13]['published'] = '';
+$page_data[13]['show_last_edit'] = '';
+$page_data[13]['linked_to_menu'] = '1';
+$page_data[13]['header_location'] = '';
+$page_data[13]['use_header_location'] = '';
+
+//----------
+$page_data[14]['url'] = 'Menu_Level_3_%E2%80%93_Page_2';
+$page_data[14]['last_edit'] = '1311883667';
+$page_data[14]['description'] = '';
+$page_data[14]['keywords'] = '';
+$page_data[14]['title'] = '';
+$page_data[14]['robots'] = '';
+$page_data[14]['heading'] = '';
+$page_data[14]['show_heading'] = '';
+$page_data[14]['template'] = '';
+$page_data[14]['published'] = '';
+$page_data[14]['show_last_edit'] = '';
+$page_data[14]['linked_to_menu'] = '1';
+$page_data[14]['header_location'] = '';
+$page_data[14]['use_header_location'] = '';
+
+//----------
+$page_data[15]['url'] = 'Menu_Level_3_%E2%80%93_Page_3';
+$page_data[15]['last_edit'] = '1311882830';
+$page_data[15]['description'] = '';
+$page_data[15]['keywords'] = '';
+$page_data[15]['title'] = '';
+$page_data[15]['robots'] = '';
+$page_data[15]['heading'] = '';
+$page_data[15]['show_heading'] = '';
+$page_data[15]['template'] = '';
+$page_data[15]['published'] = '';
+$page_data[15]['show_last_edit'] = '';
+$page_data[15]['linked_to_menu'] = '1';
+$page_data[15]['header_location'] = '';
+$page_data[15]['use_header_location'] = '';
+
+//----------
+$page_data[16]['url'] = 'Menu_Level_2_%E2%80%93_Page_3';
+$page_data[16]['last_edit'] = '1311879724';
+$page_data[16]['description'] = '';
+$page_data[16]['keywords'] = '';
+$page_data[16]['title'] = '';
+$page_data[16]['robots'] = '';
+$page_data[16]['heading'] = '';
+$page_data[16]['show_heading'] = '';
+$page_data[16]['template'] = '';
+$page_data[16]['published'] = '';
+$page_data[16]['show_last_edit'] = '';
+$page_data[16]['linked_to_menu'] = '1';
+$page_data[16]['header_location'] = '';
+$page_data[16]['use_header_location'] = '';
+
+//----------
+$page_data[17]['url'] = 'Menu_Level_3_%E2%80%93_Page_1';
+$page_data[17]['last_edit'] = '1311882916';
+$page_data[17]['description'] = '';
+$page_data[17]['keywords'] = '';
+$page_data[17]['title'] = '';
+$page_data[17]['robots'] = '';
+$page_data[17]['heading'] = '';
+$page_data[17]['show_heading'] = '';
+$page_data[17]['template'] = '';
+$page_data[17]['published'] = '';
+$page_data[17]['show_last_edit'] = '';
+$page_data[17]['linked_to_menu'] = '1';
+$page_data[17]['header_location'] = '';
+$page_data[17]['use_header_location'] = '';
+
+//----------
+$page_data[18]['url'] = 'Menu_Level_3_%E2%80%93_Page_2';
+$page_data[18]['last_edit'] = '1311882930';
+$page_data[18]['description'] = '';
+$page_data[18]['keywords'] = '';
+$page_data[18]['title'] = '';
+$page_data[18]['robots'] = '';
+$page_data[18]['heading'] = '';
+$page_data[18]['show_heading'] = '';
+$page_data[18]['template'] = '';
+$page_data[18]['published'] = '';
+$page_data[18]['show_last_edit'] = '';
+$page_data[18]['linked_to_menu'] = '1';
+$page_data[18]['header_location'] = '';
+$page_data[18]['use_header_location'] = '';
+
+//----------
+$page_data[19]['url'] = 'Menu_Level_3_%E2%80%93_Page_3';
+$page_data[19]['last_edit'] = '1311882941';
+$page_data[19]['description'] = '';
+$page_data[19]['keywords'] = '';
+$page_data[19]['title'] = '';
+$page_data[19]['robots'] = '';
+$page_data[19]['heading'] = '';
+$page_data[19]['show_heading'] = '';
+$page_data[19]['template'] = '';
+$page_data[19]['published'] = '';
+$page_data[19]['show_last_edit'] = '';
+$page_data[19]['linked_to_menu'] = '1';
+$page_data[19]['header_location'] = '';
+$page_data[19]['use_header_location'] = '';
+
+//----------
+$page_data[20]['url'] = 'Languages';
+$page_data[20]['last_edit'] = '1329938196';
+$page_data[20]['description'] = '';
+$page_data[20]['keywords'] = '';
+$page_data[20]['title'] = '';
+$page_data[20]['robots'] = '';
+$page_data[20]['heading'] = '';
+$page_data[20]['show_heading'] = '';
+$page_data[20]['template'] = '';
+$page_data[20]['published'] = '';
+$page_data[20]['show_last_edit'] = '';
+$page_data[20]['linked_to_menu'] = '1';
+$page_data[20]['header_location'] = '';
+$page_data[20]['use_header_location'] = '';
+
+//----------
+$page_data[21]['url'] = 'Templates_%26amp%3B_Plugins';
+$page_data[21]['last_edit'] = '1320934341';
+$page_data[21]['description'] = '';
+$page_data[21]['keywords'] = '';
+$page_data[21]['title'] = '';
+$page_data[21]['robots'] = '';
+$page_data[21]['heading'] = '';
+$page_data[21]['show_heading'] = '';
+$page_data[21]['template'] = '';
+$page_data[21]['published'] = '';
+$page_data[21]['show_last_edit'] = '';
+$page_data[21]['linked_to_menu'] = '1';
+$page_data[21]['header_location'] = '';
+$page_data[21]['use_header_location'] = '';
+
+//----------
+$page_data[22]['url'] = 'News01';
+$page_data[22]['last_edit'] = '1316870463';
+$page_data[22]['description'] = '';
+$page_data[22]['keywords'] = '';
+$page_data[22]['title'] = '';
+$page_data[22]['robots'] = '';
+$page_data[22]['heading'] = '';
+$page_data[22]['show_heading'] = '0';
+$page_data[22]['template'] = '0';
+$page_data[22]['published'] = '1';
+$page_data[22]['show_last_edit'] = '0';
+$page_data[22]['linked_to_menu'] = '0';
+$page_data[22]['header_location'] = '';
+$page_data[22]['use_header_location'] = '0';
+
+//----------
+$page_data[23]['url'] = 'News02';
+$page_data[23]['last_edit'] = '1316870483';
+$page_data[23]['description'] = '';
+$page_data[23]['keywords'] = '';
+$page_data[23]['title'] = '';
+$page_data[23]['robots'] = '';
+$page_data[23]['heading'] = '';
+$page_data[23]['show_heading'] = '0';
+$page_data[23]['template'] = '0';
+$page_data[23]['published'] = '1';
+$page_data[23]['show_last_edit'] = '0';
+$page_data[23]['linked_to_menu'] = '0';
+$page_data[23]['header_location'] = '';
+$page_data[23]['use_header_location'] = '0';
+
+//----------
+$page_data[24]['url'] = 'News03';
+$page_data[24]['last_edit'] = '1319015680';
+$page_data[24]['description'] = '';
+$page_data[24]['keywords'] = '';
+$page_data[24]['title'] = '';
+$page_data[24]['robots'] = '';
+$page_data[24]['heading'] = '';
+$page_data[24]['show_heading'] = '0';
+$page_data[24]['template'] = '0';
+$page_data[24]['published'] = '1';
+$page_data[24]['show_last_edit'] = '0';
+$page_data[24]['linked_to_menu'] = '0';
+$page_data[24]['header_location'] = '';
+$page_data[24]['use_header_location'] = '0';
+
+//----------
+?>
